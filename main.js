edf1d0d4b535c9ec534332db82461222c97c2a4f
MarcoPanz <marcopanzan23@gmail.com>
2024-03-28 18:43:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector('.mynav');
+
+window.addEventListener('scroll', () => {
+    let = scrolled = window.scrollY;
+    if (scrolled > 0) {
+        nav.classList.add('nav_blur')
+    }
+    else {
+
+    }
+
+})
