54c852a0b502ebe2629825676caae23fa624f8d8
MarcoPanz <marcopanzan23@gmail.com>
2024-03-28 19:38:17 +0100

    navbar finita

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 let nav = document.querySelector('.mynav');
+let linkNav = document.querySelectorAll('.nav_link');
 
 window.addEventListener('scroll', () => {
     let = scrolled = window.scrollY;
     if (scrolled > 0) {
-        nav.classList.add('nav_blur')
+        nav.classList.add('nav_blur');
+        linkNav.forEach((linkNav) => {
+            linkNav.style.color = 'var(--main-color)';
+        })
+
     }
     else {
-
+        nav.classList.remove('nav_blur');
+        linkNav.forEach((linkNav) => {
+            linkNav.style.color = 'var(--sec-color)'
+        })
     }
 
 })
